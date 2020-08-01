46329cc8bc4cb7148e6d8b962cac4c2a553b9370
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2020-08-01 15:25:15 +0300

    feat(routes): update render of home page
    Return title, styling and group names to be rendered on home page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const presetCommands = require('../common/preset-commands');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    const groupNames = Object.keys(presetCommands);
+
+    res.render('index', {
+        title: 'Which Remote?',
+        group_names: groupNames,
+        portrait_css: `.group_bar {
+            height: calc(100%/${Math.min(4, groupNames.length)});
+            line-height: calc(100vh/${Math.min(4, groupNames.length)});
+        }`,
+        landscape_css: `.group_bar {
+            height: calc(100%/${Math.min(2, groupNames.length)});
+            line-height: calc(100vh/${Math.min(2, groupNames.length)});
+        }`,
+    });
 });
 
 module.exports = router;
